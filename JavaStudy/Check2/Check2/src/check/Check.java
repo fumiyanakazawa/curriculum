42ab4b8a3b4fe7fe96e389ef2eda57f3300a68c9
fumiyanakazawa <88243091+fumiyanakazawa@users.noreply.github.com>
2021-09-04 22:21:26 +0900

    課題提出テスト
    2-Checkの作成

---
+++ JavaStudy/Check2/Check2/src/check/Check.java
@@ -0,0 +1,29 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	private static String firstName ="Nakazawa";
+	private static String lastName = "Fumiya";
+	
+	private static void printName(){
+		System.out.println(firstName + lastName);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.print("printNameメソッド → ");
+		printName();
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		robotpet.introduce();
+
+		}
+	}
+
+
+
